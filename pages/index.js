d0a1dce1aaafbf1c04637d6cd91a99a6c3b94b78
Mark Miro <markmirony+github@gmail.com>
2021-03-17 21:27:29 -0700

    Clean up home page

--- pages/index.js
+++ pages/index.js
@@ -15,12 +15,17 @@ export default function IndexPage() {
         justifyContent: "center"
       }}
     >
-      <div className="h1">🇩🇿 + 🇺🇸</div>
-      <img src="/payment-processors.png" height="48" alt="" />
-      <div className="pt-4" />
-      <h1 className="cover-heading">
-        Use American dollars to make your purchases.
-      </h1>
+      {/* <div className="h3">🇩🇿 + 🇺🇸</div> */}
+      <img
+        src="/payment-processors.png"
+        height="48"
+        alt=""
+        style={{ filter: "grayscale(0.4)", opacity: 0.6 }}
+      />
+
+      <div className="pt-3" />
+
+      <h1>Use American dollars to make your purchases.</h1>
       <p className="lead">
         The American Credit Card is a prepaid virtual card, which allows
         Algerians to make purchases anywhere in the world and on the internet.
@@ -30,19 +35,29 @@ export default function IndexPage() {
           <a className="btn btn-lg btn-primary">Request a Card →</a>
         </Link>
       </p>
-      <hr />
-      <p className="lead">
-        ${CARD_PRICE_USD} ({CARD_PRICE_DINARS} DZD) for the card plus $
-        {MONTHLY_COST_USD} per month.
-      </p>
-      <hr />
-      <p className="lead">
-        And, for the first time in Algeria, when you buy a credit card, it will
-        be activated the same day!
-      </p>
-      <hr />
-      <p className="lead">Your card is activated within a few hours.</p>
-      <hr />
+
+      <div className="pt-4" />
+
+      <div className="card bg-dark border-secondary">
+        <div className="card-header border-secondary text-center">
+          <div className="lead">
+            ${CARD_PRICE_USD} ({CARD_PRICE_DINARS} DZD) for the card plus $
+            {MONTHLY_COST_USD} per month.
+          </div>
+        </div>
+        <div className="card-header border-secondary text-center">
+          <div className="lead">
+            And, for the first time in Algeria, when you buy a credit card, it
+            will be activated the same day!
+          </div>
+        </div>
+        <div className="card-header text-center">
+          <div className="lead">Your card is activated within a few hours.</div>
+        </div>
+      </div>
+
+      <div className="pt-4" />
+
       <p>
         <Link href="/faq">
           <a className="btn btn-outline-light">Learn More →</a>
